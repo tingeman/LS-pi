40e63a43fd7fc94643b5bff6156ca8b0818ff57c
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-09-09 00:39:18 +0000

    append_as_authorized_key.sh now suggests how to test unattended access to host.

--- root/append_as_authorized_key.sh
+++ root/append_as_authorized_key.sh
@@ -3,3 +3,10 @@
 source ./cronscripter_settings
 
 cat "$SSHKEY".pub | ssh $IOT "mkdir -p ~/.ssh; touch ~/.ssh/authorized_keys; cat >> ~/.ssh/authorized_keys"
+
+if [[ $? -eq 0]]; then
+    echo "SSH key has been added to the authorized_keys of the host."
+    echo "Test access with the command: ssh -i $SSHKEY $IOT"
+else
+    echo "Could not add key to hosts authorized keys!"
+    echo "Please do it manually."
