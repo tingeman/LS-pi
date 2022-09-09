df918ec75e2e34f051da306b58b3f0b8110e1c48
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-09-09 15:11:18 +0000

    Added check for fixed ip address in append_as_authorized_key.sh

--- root/append_as_authorized_key.sh
+++ root/append_as_authorized_key.sh
@@ -11,3 +11,20 @@ else
     echo "Could not add key to hosts authorized keys!"
     echo "Please do it manually."
 fi
+
+
+dhcpd_conf=$(ssh -i $SSHKEY $IOT "cat /etc/dhcp/dhcpd.conf")
+
+if [[ -z echo "$dhcpd_conf" | grep 'LS_simulator' ]]; then
+    mac_address=`ifconfig | sed -n '/^eth0:/,/^$/ { s/ether[[:blank:]]*\([^[:blank:]]*\).*/\1/p  }'`
+    ip_address=`ifconfig | sed -n '/^eth0:/,/^$/ { s/inet [[:blank:]]*\([^[:blank:]]*\).*/\1/p  }'`    # the trailing space after 'inet' is important to distinguish from inet6
+
+    echo ' '
+    echo 'You should add the follwoing lines to the upstream server /etc/dhcp/dhcpd.conf file (sudo nano /etc/dhcp/dhcpd.conf):'
+    echo ' '
+    echo "host $hostname {"
+    echo "  hardware ethernet $(mac_address);"
+    echo "  fixed-address ${ip_address};"
+    echo "}"
+    echo ' '
+fi
