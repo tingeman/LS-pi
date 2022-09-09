b967cfc9bfc6dd1a0a7d2ff5c47cf1dc5297a456
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-09-09 01:14:06 +0000

    Making terrameter_simulator.sh react to certain commands

--- root/terrameter_simulator.sh
+++ root/terrameter_simulator.sh
@@ -1,9 +1,84 @@
 #!/bin/bash
+
+
+# Should not react to:
+#
+# SLEEP
+# WAITMEASURE
+# WAITHOUR
+# WAITHALFHOUR
+# LCDOFF
+# LCDON
+# P_NEWDA
+# NEW_PROJECT
+# UPLOAD
+# SIGNALCOMPLETE
+# SQLITE
+#
+#
+# MUST REACT TO:
+#
+# s unattendedmode 1
+# T 2x21wenner /home/root/protocols/2X21.xml  /home/root/protocols/Wenner2x21.xml 2 2 1 0 0 0
+# S 0 0 0
+# m
+# g measure
+# R
+# Q
+
+############################################
+##     ABEM Terrameter LS Console commands
+##
+##     Commands:
+##     g    Get
+##     s    Set
+##     L    List all Projects
+##     P    Create a new project
+##     O    Open a project
+##     T    Create a new task
+##     S    Create a new station
+##     W    Write settings to xml file
+##     w    Read settings from xml file
+##     R    Show recent measure results
+##     A    Show all results
+##     m    Start/Stop (m)easuring process
+##     G    Print GPS information
+##     I    Set system time from GPS
+##     U    Prepare transmitter for software update
+##     ?    Help = Show this message
+##     H    Help = Show this message
+##     Q    Quit
+##
+############################################
+
+
+
+
+source ./cronscripter_settings
+
 # Check to see if a pipe exists on stdin.
 if [[ -p /dev/stdin ]]; then
     echo "Data was piped to this script!"
     # If we want to read the input line by line
     while IFS= read line; do
+        
+        if echo ${line} | grep -Eq '^s '; then
+            echo "Simulating setting parameter..."
+        elif echo ${line} | grep -Eq '^T '; then
+            echo "Simulating creating new task..."
+        elif echo ${line} | grep -Eq '^S '; then
+            echo "Simulating creating new station..."
+        elif echo ${line} | grep -Eq '^m'; then
+            echo "Simulating starting measuring process..."
+        elif echo ${line} | grep -Eq '^g measure'; then
+            echo "Simulating getting current measure..."  
+        elif echo ${line} | grep -Eq '^R'; then
+            echo "Simulating showing current measure result..."
+        elif echo ${line} | grep -Eq '^Q'; then
+            echo "Simulating quitting the script"
+        else
+            echo "I don't know this command!"
+        fi
         echo "Line: ${line}"
     done
     # Or if we want to simply grab all the data, we can simply use cat instead
