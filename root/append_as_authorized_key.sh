39f854ca1699bdbda8a7939530ad47452ad3aa54
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-09-09 00:24:57 +0000

    Added script to append public key to upstream server authorized keys

--- root/append_as_authorized_key.sh
+++ root/append_as_authorized_key.sh
@@ -2,6 +2,4 @@
 
 source ./cronscripter_settings
 
-cat "$SSHKEY".pub | ssh $IOT "cat >> .ssh/authorized_keys"
-
-
+cat "$SSHKEY".pub | ssh $IOT "mkdir -p ~/.ssh; touch ~/.ssh/authorized_keys; cat >> ~/.ssh/authorized_keys"
